26c2b11c2ffbf41ec7dc35f98995db085c8ba45d
zhaohw <zhaohw@gospell.com>
2017-10-09 09:29:01 +0800

    constant

---
+++ src/main/java/com/zhishujiaoyu/constant/EventKeyConstant.java
@@ -0,0 +1,32 @@
+package com.zhishujiaoyu.constant;
+
+/**
+* @Description: 图文回复事件key
+* @author zhaohw
+* @date 2017/9/30 17:39
+*/
+public class EventKeyConstant {
+
+    public static final String SUBSCRIBE ="subscribe";
+
+    public static final String KEYWORD ="keyword_";
+
+    public static final String MENU_1_1 ="menu_1_1";
+    public static final String MENU_1_2 ="menu_1_2";
+    public static final String MENU_1_3 ="menu_1_3";
+    public static final String MENU_1_4 ="menu_1_4";
+    public static final String MENU_1_5 ="menu_1_5";
+
+    public static final String MENU_2_1 ="menu_2_1";
+    public static final String MENU_2_2 ="menu_2_2";
+    public static final String MENU_2_3 ="menu_2_3";
+    public static final String MENU_2_4 ="menu_2_4";
+    public static final String MENU_2_5 ="menu_2_5";
+
+    public static final String MENU_3_1 ="menu_3_1";
+    public static final String MENU_3_2 ="menu_3_2";
+    public static final String MENU_3_3 ="menu_3_3";
+    public static final String MENU_3_4 ="menu_3_4";
+    public static final String MENU_3_5 ="menu_3_5";
+
+}
